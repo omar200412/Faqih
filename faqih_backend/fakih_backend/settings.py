e4155fb030581320a4801734426078e11737fad8
omar200412 <okhalefa5@gmail.com>
2026-04-26 21:51:46 +0300

    fix: added STATIC_ROOT for collectstatic

--- faqih_backend/fakih_backend/settings.py
+++ faqih_backend/fakih_backend/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 JAZZMIN_SETTINGS = {
     "site_title": "Faqih Admin",
